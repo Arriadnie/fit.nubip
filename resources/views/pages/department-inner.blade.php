e8e07499cadcde82854fa5d6b3fe4071b74a156f
LUDA <arriadnie@gmail.com>
2020-02-08 22:31:38 +0200

    departments

--- resources/views/pages/department-inner.blade.php
+++ resources/views/pages/department-inner.blade.php
@@ -20,18 +20,8 @@
         </div>
     </div>
 
-    <div class="container-250">
-        <div class="department-list">
-            @for($i = 0; $i < 4; $i++)
-                <div class="department-item">
-                    <img src="{{ asset('/image/department1.jpg') }}" alt="">
-                    <div class="department-name">
-                        <p>Кафедра інформаційних і дистанційних технологій</p>
-                    </div>
-                </div>
-            @endfor
+    <div class="editor-content">
 
-        </div>
     </div>
 
 
