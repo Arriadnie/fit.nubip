e4b057d157185f68c1e1b4e6aec41fbcef459f19
LUDA <arriadnie@gmail.com>
2020-02-09 16:28:02 +0200

    courses slider

---
+++ resources/views/infoblocks/teacher-info/index.blade.php
@@ -0,0 +1,10 @@
+<div class="teacher-info-wrap">
+    <div class="teacher-info-image">
+        <img src="{{ asset('/image/teacher1.jpg') }}" alt="">
+    </div>
+    <div class="teacher-info">
+        <p class="teacher-status">Завідувач кафедри:</p>
+        <p class="teacher-name">Швиденко Михайло Зіновійович</p>
+        <p class="teacher-text">кандидат економічних наук, доцент, заслужений професор НУБіП України, відмінник аграрної освіти і науки України, заслужений працівник освіти України</p>
+    </div>
+</div>
