1ffcb9cb0955edc6df7fe3442cf3ce907a637e8a
LUDA <arriadnie@gmail.com>
2020-05-21 01:26:52 +0300

    404

---
+++ resources/views/includes/server-notification.blade.php
@@ -0,0 +1,23 @@
+{{--<div class="server-notification-wrap">--}}
+{{--    <div class="server-notification-item success">--}}
+{{--        <div class="icon">--}}
+{{--            <svg><use xlink:href="#success"></use></svg>--}}
+{{--        </div>--}}
+{{--        <p>Урааа, ця херня запрацювала</p>--}}
+{{--    </div>--}}
+
+{{--    <div class="server-notification-item error">--}}
+{{--        <div class="icon">--}}
+{{--            <svg><use xlink:href="#error"></use></svg>--}}
+{{--        </div>--}}
+{{--        <p>Бля, щось пішло не так</p>--}}
+{{--    </div>--}}
+
+{{--    <div class="server-notification-item info">--}}
+{{--        <div class="icon">--}}
+{{--            <svg><use xlink:href="#info"></use></svg>--}}
+{{--        </div>--}}
+{{--        <p>Шо вилупився? це тут просто так, для показухи. Іди в пєнь</p>--}}
+{{--    </div>--}}
+
+{{--</div>--}}
---
+++ resources/views/includes/vipuskik-notification.blade.php
@@ -0,0 +1,8 @@
+
+<div  data-fancybox data-src="#vipusknik-modal"  class="show-notifications active">
+    <svg><use xlink:href="#question"></use></svg>
+</div>
+
+<div style="display: none" id="vipusknik-modal" class="notification-modal">
+
+</div>
