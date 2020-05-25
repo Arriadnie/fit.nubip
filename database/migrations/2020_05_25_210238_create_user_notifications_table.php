d9dbc17463aa47908014350efa4fb7a32a7b1551
ltvladislav <vlad_litvinchyk@ukr.net>
2020-05-26 00:15:22 +0300

    Add notification table and schedule DB

---
+++ database/migrations/2020_05_25_210238_create_user_notifications_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_notifications', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('message')->nullable();
+            $table->boolean('is_shown')->default(false);
+            $table->string('subject')->nullable();
+            $table->string('record_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onUpdate('cascade')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_notifications');
+    }
+}
